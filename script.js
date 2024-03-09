e77824ba478698ec7e16519a3a5bda73ca1131aa
nikakupr <130072114+nikakupr@users.noreply.github.com>
2024-03-09 03:36:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// homework
+
+// ----- task 1
+
+let x = 100;
+
+if (x < 50) {
+  console.log(x + " " + "is less than 50");
+} else if (x > 20) {
+  console.log(x + " " + "is greater than 20");
+} else {
+  console.log("error");
+}
+
+// ----- task 2
+
+let userName = "Mariami";
+
+let userNameChecker = (userName = "Mariami" ? true : false);
+console.log(userNameChecker);
+
+// ----- task 3
+
+let userName2 = "Mariami";
+
+switch (userName2) {
+  case userName2:
+    "Mariami";
+    console.log(true);
+    break;
+  default:
+    console.log(false);
+    break;
+}
